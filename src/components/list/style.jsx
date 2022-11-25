249cbc259ae8ce49f045be98ebd3a608ce84132f
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-11-25 19:17:58 -0300

    Style: Mudei a cor da fonte

--- src/components/list/style.jsx
+++ src/components/list/style.jsx
@@ -23,11 +23,16 @@ export const HeaderListTasksDone = styled.div`
   justify-content: flex-start;
   gap: 8px;
   align-items: center;
-
+  color: #303030;
   padding: 8px 12px 8px 0px;
   border-bottom: ${({ tasksDoneisVisible }) =>
     tasksDoneisVisible === true ? 'none' : '0.5px solid #bbb'
   };
+
+  h4{
+    font-size: 14px;
+    font-weight: 600;
+  }
 
   img{
     transform: rotate( ${({ tasksDoneisVisible }) =>
