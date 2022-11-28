375da1602693f8f03b94fbd428ae58330950758f
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-11-28 15:42:58 -0300

    Chore: Add o modal ao componente principal

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -4,24 +4,25 @@ import { Header } from "../header";
 import { TodoForm } from "../TodoForm";
 import { List } from "../list";
 import { SearchedTasks } from "../searchedTasks";
+import { ModalModifyTask } from '../modalModifyTask';
 
 export function Page() {
-  const { modeSearch } = useContext(TodoContext)
+  const { modeSearch, modalModifyTaskIsVisible } = useContext(TodoContext);
 
   return (
     <section>
       <Header />
-      
+
       {modeSearch ? (
-        <>
           <SearchedTasks />
-        </>
       ) : (
         <>
           <TodoForm />
           <List />
         </>
       )}
+
+      {modalModifyTaskIsVisible && <ModalModifyTask />}
     </section>
   );
 }
